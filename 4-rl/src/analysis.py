ba74d39e2d581c4b0ab725491c60b38d85f80e0e
matias <matiasnieto1419@gmail.com>
2023-12-02 14:29:52 +0100

    analysis.py terminer

--- 4-rl/src/analysis.py
+++ 4-rl/src/analysis.py
@@ -2,26 +2,31 @@ from dataclasses import dataclass
 
 @dataclass
 class Parameters:
-    reward_live: float  # Can be any value.
-    gamma: float        # Should be between 0 and 1.
-    noise: float        # Should be between 0 and 1.
+    reward_live: float
+    gamma: float
+    noise: float
 
 def prefer_close_exit_following_the_cliff() -> Parameters:
-    # High negative reward to drive the agent quickly to the exit, high gamma for future reward, low noise
-    return Parameters(reward_live=-0.9, gamma=0.95, noise=0.05)
+    # Préférer la sortie proche (+1) en longeant la falaise.
+    reward_live, gamma, noise = -0.9, 0.9, 0.5
+    return Parameters(reward_live, gamma, noise)
 
 def prefer_close_exit_avoiding_the_cliff() -> Parameters:
-    # Less negative reward for living to ensure the agent avoids the cliff, high gamma, low noise
-    return Parameters(reward_live=-0.2, gamma=0.95, noise=0.05)
+    # Préférer la sortie proche (+1) en évitant la falaise.
+    reward_live, gamma, noise = -0.9, 0.5, 0.9
+    return Parameters(reward_live, gamma, noise)
 
 def prefer_far_exit_following_the_cliff() -> Parameters:
-    # Positive reward for living to motivate reaching the far exit, very high gamma, moderate noise
-    return Parameters(reward_live=0.01, gamma=0.99, noise=0.15)
+    # Préférer la sortie distante (+10) en longeant la falaise.
+    reward_live, gamma, noise = -0.5, 0.5, 0.1
+    return Parameters(reward_live, gamma, noise)
 
 def prefer_far_exit_avoiding_the_cliff() -> Parameters:
-    # Positive living reward, very high gamma to prioritize distant rewards, low noise to avoid cliff
-    return Parameters(reward_live=0.01, gamma=0.99, noise=0.05)
+    # Préférer la sortie distante (+10) en évitant la falaise.
+    reward_live, gamma, noise = 0.9, 0.5, 0.5
+    return Parameters(reward_live, gamma, noise)
 
 def never_end_the_game() -> Parameters:
-    # High positive living reward to keep the game going, gamma of 1 to not discount future rewards, moderate noise
-    return Parameters(reward_live=0.1, gamma=1.0, noise=0.2)
+    #  Eviter de terminer le jeu (l’épisode ne se termine jamais).
+    reward_live, gamma, noise = 1, 1, 1
+    return Parameters(reward_live, gamma, noise)
