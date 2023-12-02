1df9cf562084f8c4e7c22f128c92808900a77874
matias <matiasnieto1419@gmail.com>
2023-12-02 13:13:34 +0100

    Value iteration Ok opti

--- 4-rl/src/analysis.py
+++ 4-rl/src/analysis.py
@@ -1,31 +1,27 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class Parameters:
-    reward_live: float
-    """Reward for living at each time step"""
-    gamma: float
-    """Discount factor"""
-    noise: float
-    """Probability of taking a random action instead of the chosen one"""
-
+    reward_live: float  # Can be any value.
+    gamma: float        # Should be between 0 and 1.
+    noise: float        # Should be between 0 and 1.
 
 def prefer_close_exit_following_the_cliff() -> Parameters:
-    return Parameters(...)
-
+    # High negative reward to drive the agent quickly to the exit, high gamma for future reward, low noise
+    return Parameters(reward_live=-0.9, gamma=0.95, noise=0.05)
 
 def prefer_close_exit_avoiding_the_cliff() -> Parameters:
-    return Parameters(...)
-
+    # Less negative reward for living to ensure the agent avoids the cliff, high gamma, low noise
+    return Parameters(reward_live=-0.2, gamma=0.95, noise=0.05)
 
 def prefer_far_exit_following_the_cliff() -> Parameters:
-    return Parameters(...)
-
+    # Positive reward for living to motivate reaching the far exit, very high gamma, moderate noise
+    return Parameters(reward_live=0.01, gamma=0.99, noise=0.15)
 
 def prefer_far_exit_avoiding_the_cliff() -> Parameters:
-    return Parameters(...)
-
+    # Positive living reward, very high gamma to prioritize distant rewards, low noise to avoid cliff
+    return Parameters(reward_live=0.01, gamma=0.99, noise=0.05)
 
 def never_end_the_game() -> Parameters:
-    return Parameters(...)
+    # High positive living reward to keep the game going, gamma of 1 to not discount future rewards, moderate noise
+    return Parameters(reward_live=0.1, gamma=1.0, noise=0.2)
--- 4-rl/src/value_iteration.py
+++ 4-rl/src/value_iteration.py
@@ -1,27 +1,35 @@
 from mdp import MDP, S, A
-from typing import Generic
-
+from typing import Generic, Dict
 
 class ValueIteration(Generic[S, A]):
     def __init__(self, mdp: MDP[S, A], gamma: float):
-        # senf.values est nécessaire pour fonctionner avec utils.show_values
-        self.values = dict[S, float]()
+        self.gamma = gamma  # Discount factor
+        self.mdp = mdp  # The Markov Decision Process
+        self.values = {state: 0.0 for state in self.mdp.states()}  # Initialize state values
 
     def value(self, state: S) -> float:
         """Returns the value of the given state."""
+        return self.values.get(state, 0.0)
 
     def policy(self, state: S) -> A:
         """Returns the action that maximizes the Q-value of the given state."""
+        best_action = max(self.mdp.available_actions(state),
+                          key=lambda action: self.qvalue(state, action))
+        return best_action
 
     def qvalue(self, state: S, action: A) -> float:
         """Returns the Q-value of the given state-action pair based on the state values."""
+        return sum(probability * (self.mdp.reward(state, action, new_state) + self.gamma * self.value(new_state))
+                   for new_state, probability in self.mdp.transitions(state, action))
 
     def _compute_value_from_qvalues(self, state: S) -> float:
-        """
-        Returns the value of the given state based on the Q-values.
-
-        This is a private method, meant to be used by the value_iteration method.
-        """
+        """Computes the value for a state based on its Q-values."""
+        if self.mdp.is_final(state):
+            return 0.0
+        return max(self.qvalue(state, action) for action in self.mdp.available_actions(state))
 
     def value_iteration(self, n: int):
-        ...
+        for _ in range(n):
+            for state in self.mdp.states():
+                self.values[state] = self._compute_value_from_qvalues(state)
+
