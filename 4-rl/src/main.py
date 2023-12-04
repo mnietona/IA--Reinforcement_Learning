5aa531302420244bec977458b9c93877fdf89d24
matias <matiasnieto1419@gmail.com>
2023-12-04 20:24:12 +0100

    Lvl 1 ok il apprend bien

--- 4-rl/src/main.py
+++ 4-rl/src/main.py
@@ -2,35 +2,72 @@ from lle import LLE
 from rlenv.wrappers import TimeLimit
 from qlearning import QLearning 
 
-alpha = 0.1    
-gamma = 0.9    
-epsilon = 0.3
-episodes = 10
+alpha = 0.1    # Taux d'apprentissage
+gamma = 0.9    # Facteur de remise
+epsilon = 1.0  # Commence avec une exploration complète
+epsilon_min = 0.1  # Valeur minimale pour epsilon
+epsilon_decay = 0.995  # Facteur de décroissance d'epsilon
+episodes = 5000
 
-env = TimeLimit(LLE.level(1), 5) 
+env = TimeLimit(LLE.level(1), 100)
 
-Action = ["NORTH (UP)", "SOUTH (DOWN)", "EAST (RIGHT)","WEST (LEFT)", "STAY"]
+Action = ["NORTH (UP)", "SOUTH (DOWN)", "EAST (RIGHT)", "WEST (LEFT)", "STAY"]
 
 agents = [QLearning(alpha, gamma, epsilon) for _ in range(env.n_agents)]
-# Entrainment
-print("Entrainement")
-observation = env.reset()
-done = truncated = False
-score = 0
+# Entraînement
+print("Entraînement")
+scores = []  # Pour suivre l'évolution du score
 
 for episode in range(episodes):
-    while not done :
-        actions = [a.choose_action(observation) for a in agents]
-        print(f"Actions : {Action[actions[0]]}")
-        next_observation, reward, done, truncated, info = env.step(actions)
-        print(f"Reward: {reward}, info: {info}, dones: {done}, truncated: {truncated}")
+    observation = env.reset()
+    done = truncated = False
+    score = 0
 
-        for a in agents:
-            a.update(observation, actions, reward, next_observation)
+    while not (done or truncated):
+        actions = [agent.choose_action(observation) for agent in agents]
+
+        next_observation, reward, done, truncated, info = env.step(actions)
+
+        for agent in agents:
+            agent.update(observation, actions, reward, next_observation)
         
         observation = next_observation
         score += reward
-    
-    print(f"Episode {episode} - Score: {score}")
 
-agents[0].print_q_table()
+    scores.append(score)
+    epsilon = max(epsilon_min, epsilon_decay * epsilon)  # Réduit epsilon
+    #print(f"Episode {episode} - Score: {score}, Epsilon: {epsilon}")
+
+    # Mise à jour des agents avec le nouvel epsilon
+    for agent in agents:
+        agent.epsilon = epsilon
+
+# Affichage de la table Q de l'un des agents
+#agents[0].print_q_table()
+
+#Analyse du score
+average_score = sum(scores) / len(scores)
+print(f"Score moyen sur {episodes} épisodes: {average_score}")
+
+# Joue le monde 
+
+observation = env.reset()
+done = truncated = False
+score = 0
+step = 0
+gems = 0
+agents[0].epsilon = 0.0
+
+while not done:
+    
+    actions = [agent.choose_action(observation) for agent in agents]
+    next_observation, reward, done, truncated, info = env.step(actions)
+
+    observation = next_observation
+    score += reward
+    step += 1
+
+    
+
+print(f"Step {step} - Score: {score}, Gems: {info.get('gems_collected')}")
+
--- 4-rl/src/qlearning.py
+++ 4-rl/src/qlearning.py
@@ -1,10 +1,8 @@
 from rlenv import RLEnv, Observation
 import numpy as np
-import numpy.typing as npt
 import random
 
 class QLearning:
-
     def __init__(self, alpha=0.1, gamma=0.9, epsilon=0.1, n_actions=5):
         self.q_table = {}  # Dictionnaire pour les valeurs Q
         self.alpha = alpha
@@ -14,47 +12,48 @@ class QLearning:
 
     def choose_action(self, observation: Observation):
         state = observation.state
-        state_key = hash(state.tobytes())
+        state_key = self._get_state_key(state)
 
+        # On suppose que `available_actions` est un tableau binaire de la forme (n_actions,)
+        # où 1 indique que l'action est disponible et 0 sinon.
         available_actions = observation.available_actions[0]
         available_actions_indices = np.where(available_actions > 0)[0]
 
         if state_key not in self.q_table:
+            # Initialiser avec des valeurs de 1 pour encourager l'exploration
             self.q_table[state_key] = np.ones(self.n_actions)
 
         if random.uniform(0, 1) < self.epsilon:  # Exploration
             action = np.random.choice(available_actions_indices)
-            return action
         else:  # Exploitation
-            q_values = self.q_table[state_key]
-            q_values_filtered = q_values[available_actions_indices]
-            best_action_index = np.argmax(q_values_filtered)
-            return available_actions_indices[best_action_index]
+            q_values = self.q_table[state_key][available_actions_indices]
+            best_action_index = np.argmax(q_values)
+            action = available_actions_indices[best_action_index]
+
+        return action
 
     def update(self, observation, action, reward, next_observation):
         state = observation.state
         next_state = next_observation.state
 
-        state_key = hash(state.tobytes())
-        next_state_key = hash(next_state.tobytes())
+        state_key = self._get_state_key(state)
+        next_state_key = self._get_state_key(next_state)
 
         if next_state_key not in self.q_table:
             self.q_table[next_state_key] = np.ones(self.n_actions)
 
-        # Update Q 
-        current_q = self.q_table[state_key]
-        next_q = self.q_table[next_state_key]
-        old_value = current_q[action]
-        next_max = np.max(next_q)
-        print(f"old_value: {old_value}, next_max: {next_max}, reward: {reward}")
+        # Mise à jour de Q
+        old_value = self.q_table[state_key][action]
+        next_max = np.max(self.q_table[next_state_key])
         new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
 
-        current_q[action] = new_value
-        self.q_table[state_key] = current_q
+        self.q_table[state_key][action] = new_value
 
+    def _get_state_key(self, state):
+        """ Génère une clé unique pour l'état donné pour être utilisée dans la table Q. """
+        return hash(state.tobytes())
 
     def print_q_table(self):
-        for state_key in self.q_table:
-            print(f"State {state_key}: {self.q_table[state_key]}")
-        
+        for state_key, values in self.q_table.items():
+            print(f"State {state_key}: {values}")
         print(f"Q-table size: {len(self.q_table)}")
