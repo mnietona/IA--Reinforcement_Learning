6009f1f1f663674cae6c717c699886e814a4f7a9
matias <matiasnieto1419@gmail.com>
2023-12-14 14:51:56 +0100

    V final

--- 4-rl/src/analysis.py
+++ 4-rl/src/analysis.py
@@ -11,20 +11,20 @@ class Parameters:
 
 def prefer_close_exit_following_the_cliff() -> Parameters:
     # Préférer la sortie proche (+1) en longeant la falaise.
-    return Parameters(reward_live = -0.9, gamma = 0.9, noise = 0.5)
+    return Parameters(reward_live = -0.9, gamma = 0.5, noise = 0.1)
 
 def prefer_close_exit_avoiding_the_cliff() -> Parameters:
     # Préférer la sortie proche (+1) en évitant la falaise.
-    return Parameters(reward_live = -0.9, gamma = 0.5, noise = 0.9)
+    return Parameters(reward_live = -0.9, gamma = 0.1, noise = 0.9)
 
 def prefer_far_exit_following_the_cliff() -> Parameters:
     # Préférer la sortie distante (+10) en longeant la falaise.
-    return Parameters(reward_live = -0.5, gamma = 0.9, noise = 0.1)
+    return Parameters(reward_live = 0.3, gamma = 0.9, noise = 0.1)
 
 def prefer_far_exit_avoiding_the_cliff() -> Parameters:
     # Préférer la sortie distante (+10) en évitant la falaise.
-    return Parameters(reward_live = 0.5 , gamma = 0.9, noise = 0.1)
+    return Parameters(reward_live = 0.3, gamma = 0.5, noise = 0.9)
 
 def never_end_the_game() -> Parameters:
     #  Eviter de terminer le jeu (l’épisode ne se termine jamais).
-    return Parameters(reward_live = 1, gamma = 0.9, noise = 1)
+    return Parameters(reward_live = 1, gamma = 1, noise = 1)
